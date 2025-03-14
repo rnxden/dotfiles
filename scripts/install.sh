cea3f02c880b4a49f3c0d260e0b9cb8fdacb879e
Arden X. <rnx@rnx.sh>
2025-03-14 07:33:51 -0400

    build: add makefile and install script
    Creates user config directory and installs zsh through symlinks.

---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Creates a symlink, keeping a backup if a file previously existed
+symlink() {
+  if [[ -e "$2" ]]; then
+    if [[ -L "$2" ]]; then
+      echo "Existing symlink found at $2, deleting..."
+      rm -f "$2"
+    else
+      echo "Existing file or directory found at $2, creating backup..."
+      mv -f "$2" "${2}_$(date +%s).bak"
+    fi
+  fi
+
+  echo "Creating symlink to $1 at $2..."
+  ln -sf "$1" "$2"
+}
+
+# Create config directory
+CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}"
+if [[ ! -d "$CONFIG" ]]; then
+  echo "Config directory not found, creating..."
+  mkdir -p "$CONFIG"
+fi
+
+# Create symlinks for zsh
+symlink "$PWD/zsh/.zshrc" "$HOME/.zshrc"
+symlink "$PWD/zsh/.zimrc" "$HOME/.zimrc"
