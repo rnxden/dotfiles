f922276d2ee0199720bd5530845cb09c64de9c1f
Arden X. <rnx@rnx.sh>
2025-06-09 21:00:06 -0400

    feat(nvim): remove unused plugin vim-illuminate

--- nvim/lua/rnx/plugins/list/visuals.lua
+++ nvim/lua/rnx/plugins/list/visuals.lua
@@ -17,36 +17,6 @@ return {
     end,
   },
 
-  -- Document highlights
-  {
-    'RRethy/vim-illuminate',
-
-    event = 'BufReadPost',
-
-    keys = {
-      {
-        ']]',
-        function()
-          require('illuminate').goto_next_reference()
-        end,
-      },
-      {
-        '[[',
-        function()
-          require('illuminate').goto_prev_reference()
-        end,
-      },
-    },
-
-    opts = {
-      providers = { 'lsp', 'treesitter' },
-    },
-
-    config = function(_, opts)
-      require('illuminate').configure(opts)
-    end,
-  },
-
   -- Sign column git status
   {
     'lewis6991/gitsigns.nvim',
