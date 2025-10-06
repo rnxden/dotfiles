7a38b0bc0e6d78b8fada570c1aa6f5ef41c618a8
Arden X <rnx@rnx.sh>
2025-10-05 23:14:46 -0400

    feat(nvim): add zenbones (zenwritten) colorscheme

--- nvim/lua/rnx/plugins/list/colorschemes.lua
+++ nvim/lua/rnx/plugins/list/colorschemes.lua
@@ -23,4 +23,26 @@ return {
       require('vague').setup(opts)
     end,
   },
+
+  -- Minimal constrast-based colorscheme
+  {
+    'zenbones-theme/zenbones.nvim',
+
+    dependencies = { 'rktjmp/lush.nvim' },
+
+    --[[
+    init = function()
+      vim.g.zenwritten_italic_comments = false
+      vim.g.zenwritten_italic_strings = false
+
+      vim.cmd.colorscheme('zenwritten')
+
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+    end,
+    -]]
+  },
 }
